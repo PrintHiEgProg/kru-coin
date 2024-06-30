5ad0f745175d08280b38177ace3d1918f6a207ef
PrintHiEgProg <egorkobzev23@gmail.com>
2024-06-30 23:13:20 +0300

    integration tg try 3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import RefLink from "./RefLink.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const tg = window.Telegram.WebApp;
+  const userId = tg.initDataUnsafe.user.id;
   const [count, setCount] = useState(() => {
     const savedCount = localStorage.getItem("count");
     return savedCount !== null ? parseInt(savedCount, 10) : 0;
@@ -82,36 +84,6 @@ function App() {
     // Здесь можно добавить логику для загрузки данных с сервера или другие операции
   }, []);
 
-  const [userId, setUserId] = useState(""); // Используем state для хранения ID пользователя
-
-  // Функция для получения ID пользователя
-  const getUserId = async () => {
-    try {
-      // Здесь вам нужно будет заменить 'YOUR\_BOT\_TOKEN' на токен вашего бота
-      const response = await fetch(
-        `https://api.telegram.org/bot7018807448:AAFwKDKpTX7QJbh1EXAwCIq7V_0lZiKyzoY/getMe`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await response.json();
-      setUserId(data.result.id); // Сохраняем ID в state
-    } catch (error) {
-      console.error("Ошибка при получении ID пользователя:", error);
-    }
-  };
-
-  // Вызываем функцию при загрузке компонента
-  useEffect(() => {
-    getUserId();
-  }, []);
-
-
-
-
 
   return (
     <div className="App">
@@ -143,7 +115,7 @@ function App() {
             />
             <Route path="/boost" element={<Boost count={count} />} />
             <Route path="/task" element={<Task />} />
-              <Route path="/link" element={<RefLink userId={userId} />} />
+              <Route path="/link" element={<RefLink userId={userId}/>} />
           </Routes>
           <NavBar />
         </Router>
--- src/RefLink.js
+++ src/RefLink.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function RefLink({ UserId }) {
+function RefLink({ userId}) {
 
   return (
     <div className='RefLink'>
       <h2 className='title'>RefLink</h2>
-      <h2>{UserId}</h2>
+      <h2 className='title'>{userId}</h2>
     </div>
   );
   
